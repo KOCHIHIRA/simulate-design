b98e5908515549c2e24470ccc475d0fd8d12b7cf
s18007 <s18007@std.it-college.ac.jp>
2021-10-20 00:08:40 +0900

    Mainファイルの追加

---
+++ src/template/Main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Main(props) {
+
+    if(this.props.name) {
+        return(
+            <>
+                
+            </>
+        )
+    }
+}
+
+export default Main;
