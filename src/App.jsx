fa988080a489be7429168fd07f95b30bf649f9a6
s18007 <s18007@std.it-college.ac.jp>
2021-10-20 00:08:00 +0900

    Appファイルの追加

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import SideBar from "./template/SideBar";
+
+//AppはSideBarとMainをまとめる役割
+function App() {
+    return(
+        <SideBar />
+    )
+}
+
+export default App;
